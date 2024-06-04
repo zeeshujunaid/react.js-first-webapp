d401a4a3942db1f3733b92b09fe8409d6bf753c4
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-06-05 02:13:03 +0500

    x

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import {db} from "./database/firebase"
-import { collection, addDoc } from "firebase/firestore"; 
+import {useState, useEffect} from "react"
+import { db } from "./database/firebase"
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 
 function App() {
+  var users = collection(db, "users")
+  useEffect(()=>{
+    getallusers()
+    console.log("zeeshan");
+  }, [])
+
+  function addDatatofirebase() {
+    var data = { name: "zeeshan", age: "20" }
+    addDoc(users, data)
+
+  }
+  async function getallusers() {
+    const querySnapshot = await getDocs(users);
+    var list = []
+    querySnapshot.forEach((doc) => {
+      list.push(doc.data())
+      console.log (doc.data());
+    });
+    console.log (list);
+  }
+
+
   return (
-    <div><h1>hi this is my first code in react.js</h1></div>
+    <div>
+      <h1>hi this is my first code in react.js</h1>
+      <button onClick={addDatatofirebase}>add data to firebase</button>
+    </div>
   );
 }
 
